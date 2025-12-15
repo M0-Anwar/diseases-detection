5bf6cb1a354f05f5014c71eb9fec832659ffb0b1
M0-Anwar <moanwer.dev@gmail.com>
2025-12-15 11:09:38 +0200

    change the api from local host to the deployed host

--- frontEnd/src/pages/InputPage.tsx
+++ frontEnd/src/pages/InputPage.tsx
@@ -22,7 +22,7 @@ export default function InputPage() {
         setError(""); setLoading(true)
 
         try {
-            const response = await fetch("http://127.0.0.1:8000/api/predict/list", {
+            const response = await fetch("https://diseases-detection-wdqcoa.fly.dev/api/predict/list", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ snp_list: mutationList })
