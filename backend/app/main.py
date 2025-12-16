5d37717caf05f3635ac6c9861db62b1e8a220ca7
M0-Anwar <moanwer.dev@gmail.com>
2025-12-16 12:22:26 +0200

    fix app file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,26 @@
-c
+import os
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes import router
+
+app = FastAPI(title="Disease SNP Prediction API")
+
+origins = [
+    "http://localhost:3000",
+    "https://diseases-detection-b789.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
